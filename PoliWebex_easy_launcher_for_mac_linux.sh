2b1de0ac78f481b778fe7ba3bd673a255a189494
1alessandro1 <46293832+1alessandro1@users.noreply.github.com>
2022-02-19 22:04:18 +0100

    Add simple bash script for Mac-Linux for PoliWebex too
    Users can drag their directories onto the prompt

---
+++ PoliWebex_easy_launcher_for_mac_linux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Remember! Assignment is space-sensitive!
+
+# Uncomment below if you permanently set a path for your poliwebex.js and node_modules folder
+
+# poliwebex_folder_path="/home/$USER/Videos/PoliWebex/scripts"
+
+read -p 'Drag here the directory where polidown.js is located (no_spaces_please): ' poliwebex_folder_path
+
+read -p 'Drag here the URLs.txt formatted with one link per line (no_spaces_please): ' urls_file_path
+
+read -p 'Enter the dest. path where you want to put your recordings (no_spaces_please): ' output_folder_path
+
+cd $poliwebex_folder_path
+
+node poliwebex.js -f $urls_file_path -o $output_folder_path
+
+read -n 1 -s -r -p "Press any key to terminate..."
